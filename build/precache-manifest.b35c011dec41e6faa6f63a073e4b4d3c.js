e2bfc26675ec701183ee11923a9404967b67ecac
Mehmet D <mehmettaskaya15@gmail.com>
2021-06-05 20:27:54 +0300

    A working coin tosser game

---
+++ build/precache-manifest.b35c011dec41e6faa6f63a073e4b4d3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6ffd0a4f1687f73f06999a577b2975e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63e73fe0e2e224dd1190",
+    "url": "/static/css/main.c9c0c095.chunk.css"
+  },
+  {
+    "revision": "6f46b9c97abacbd0f2be",
+    "url": "/static/js/2.2ecc874f.chunk.js"
+  },
+  {
+    "revision": "63e73fe0e2e224dd1190",
+    "url": "/static/js/main.b71d618e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7c42669a738f1a1e15ef7ccedaaa8ed2",
+    "url": "/static/media/1-lira-tura.7c42669a.png"
+  },
+  {
+    "revision": "9b70882eb706071cc877c4040c16702d",
+    "url": "/static/media/1-lira-yazi.9b70882e.png"
+  }
+]);
--- src/components/CoinFlipper/CoinFlipper.js
+++ src/components/CoinFlipper/CoinFlipper.js
@@ -1,3 +1,44 @@
+// import React, { Component } from "react";
+// import Coin from "../Coin/Coin";
+// import "./CoinFlipper.css";
+
+// class CoinFlipper extends Component {
+//   constructor(props) {
+//     super(props);
+//     // State üzerinde paranın başlangıçtaki durumunu veriyoruz, başlangıçta "tura" olsun.
+//     // Daha sonra şu anda paranın dönüp dönmeme durumunu da veriyoruz, başlangıçta para atılmamış olduğundan "false" olarak verdik.
+//     this.state = {
+//       side: "tura",
+//       flipping: false,
+//     };
+//   }
+//   handleClick = () => {
+//     // "At!" butonuna tıkladığımızda paranın dönmesini istiyoruz, bu yüzden "flipping" durumunu "true" yapıyoruz.
+//     this.setState({ flipping: true });
+//     // 1 saniye kadar dönmesi yeterli, bu yüzden 1 saniye sonra "flipping" durmunu tekrar "false" yapıyoruz.
+//     setTimeout(() => this.setState({ flipping: false }), 1000);
+//   };
+
+//   render() {
+//     return (
+//       <div className="CoinFlipper">
+//         <h1>Yazı mı Tura mı?</h1>
+//         <Coin side={this.state.side} flipping={this.state.flipping} />
+//         <button onClick={this.handleClick}>At!</button>
+//         <p>
+//           Toplam
+//           <strong> 5 </strong>
+//           atıştan
+//           <strong> 3 </strong>ü tura
+//           <strong> 2 </strong>
+//           si yazı geldi.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default CoinFlipper;
 import React, { Component } from "react";
 import Coin from "../Coin/Coin";
 import "./CoinFlipper.css";
@@ -5,17 +46,32 @@ import "./CoinFlipper.css";
 class CoinFlipper extends Component {
   constructor(props) {
     super(props);
-    // State üzerinde paranın başlangıçtaki durumunu veriyoruz, başlangıçta "tura" olsun.
-    // Daha sonra şu anda paranın dönüp dönmeme durumunu da veriyoruz, başlangıçta para atılmamış olduğundan "false" olarak verdik.
     this.state = {
       side: "tura",
       flipping: false,
+      total: {
+        totalSide: 0,
+        turaSide: 0,
+        yaziSide: 0,
+      },
     };
   }
   handleClick = () => {
-    // "At!" butonuna tıkladığımızda paranın dönmesini istiyoruz, bu yüzden "flipping" durumunu "true" yapıyoruz.
+    this.state.total.totalSide++;
+    let randomSide = Math.floor(Math.random() * 2);
+
+    setTimeout(() => {
+      if (randomSide > 0) {
+        this.setState({ side: "yazi" });
+        this.state.total.yaziSide++;
+      } else {
+        this.setState({ side: "tura" });
+        this.state.total.turaSide++;
+      }
+    }, 500);
+
     this.setState({ flipping: true });
-    // 1 saniye kadar dönmesi yeterli, bu yüzden 1 saniye sonra "flipping" durmunu tekrar "false" yapıyoruz.
+
     setTimeout(() => this.setState({ flipping: false }), 1000);
   };
 
@@ -24,14 +80,20 @@ class CoinFlipper extends Component {
       <div className="CoinFlipper">
         <h1>Yazı mı Tura mı?</h1>
         <Coin side={this.state.side} flipping={this.state.flipping} />
-        <button onClick={this.handleClick}>At!</button>
+        <button className="btn" onClick={this.handleClick}>
+          At!
+        </button>
         <p>
           Toplam
-          <strong> 5 </strong>
+          <strong> {this.state.total.totalSide} </strong>
           atıştan
-          <strong> 3 </strong>ü tura
-          <strong> 2 </strong>
-          si yazı geldi.
+        </p>
+        <p>
+          <strong> {this.state.total.turaSide} </strong> tura
+        </p>
+        <p>
+          <strong> {this.state.total.yaziSide} </strong>
+          yazı geldi
         </p>
       </div>
     );
